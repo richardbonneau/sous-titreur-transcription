9023a777192e6f7feda68be796a571bcfd741e6e
richardbonneau <36018812+richardbonneau@users.noreply.github.com>
2021-07-15 12:47:31 -0400

    word detection and application is much better

--- src/Sections/Transcription.js
+++ src/Sections/Transcription.js
@@ -26,6 +26,7 @@ const Container = styled.div`
     z-index: 100;
     left: ${(props) => props.caretPosition.left};
     top: ${(props) => props.caretPosition.top};
+    display: ${(props) => (props.isMenuActive ? "block" : "none")};
   }
 `;
 
@@ -54,28 +55,40 @@ function Transcription() {
   }, []);
 
   useEffect(() => {
-    const textToArray = transcriptionText.split(" ");
+    const textToArray = transcriptionText.split(/\n| /g);
     const lastWord = textToArray[textToArray.length - 1];
-
+    const lastCharacter = transcriptionText[transcriptionText.length - 1];
+    console.log("textToArray", textToArray);
     frequentlyUsedWords.forEach((word) => {
       const firstTwoCharacters = word.substring(0, 2);
-      if (lastWord.toLowerCase().includes(firstTwoCharacters.toLowerCase()) && lastWord.toLowerCase() !== word.toLowerCase()) {
-        console.log("oi",lastWord.toLowerCase());
+      if (
+        lastCharacter !== " " &&
+        lastWord.toLowerCase().includes(firstTwoCharacters.toLowerCase()) &&
+        lastWord.toLowerCase() !== word.toLowerCase()
+      ) {
+        console.log("oi", lastWord.toLowerCase());
         setContextMenu([word]);
-
-      } 
+      }
     });
   }, [transcriptionText]);
 
   const contextOptionClicked = (word) => {
-    const lastIndex = transcriptionText.lastIndexOf(" ");
+    const lastIndexOfSpace = transcriptionText.lastIndexOf(" ");
+    const lastIndexOfBreakLine = transcriptionText.lastIndexOf("\n");
+    const lastIndex = Math.max(lastIndexOfSpace, lastIndexOfBreakLine);
+    let extraSpace = lastIndex === lastIndexOfSpace && lastIndex !== -1 ? " " : "\n";
+
+    if (lastIndex !== lastIndexOfSpace) extraSpace = "\n";
+    else if (lastIndex === -1) extraSpace = "";
+
     const originalTextWithoutLastWord = transcriptionText.substring(0, lastIndex);
-    setContextMenu([])
-    setTranscriptionText(originalTextWithoutLastWord + " " + word);
+
+    setContextMenu([]);
+    setTranscriptionText(originalTextWithoutLastWord + extraSpace + word);
   };
 
   return (
-    <Container caretPosition={caretPosition.current}>
+    <Container caretPosition={caretPosition.current} isMenuActive={contextMenu.length > 0}>
       <textarea
         type="text"
         onChange={(e) => {
@@ -85,8 +98,8 @@ function Transcription() {
       />
 
       <Menu className="context-menu">
-        {contextMenu.map((word) => (
-          <MenuItem text={word} onClick={() => contextOptionClicked(word)} />
+        {contextMenu.map((word,i) => (
+          <MenuItem key={"context"+i} text={word} onClick={() => contextOptionClicked(word)} />
         ))}
       </Menu>
     </Container>
